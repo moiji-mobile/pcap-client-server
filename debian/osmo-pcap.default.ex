821f4fad6803525bb5b4973c2e19b94aeb2ae3f5
Holger Hans Peter Freyther <zecke@selfish.org>
2011-06-01 15:00:01 +0200

    debian: Start packaging things

---
+++ debian/osmo-pcap.default.ex
@@ -0,0 +1,10 @@
+# Defaults for osmo-pcap initscript
+# sourced by /etc/init.d/osmo-pcap
+# installed at /etc/default/osmo-pcap by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
