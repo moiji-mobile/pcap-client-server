ea4ad4680a9ca3a28dfb1262baeed6da573db94d
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2016-04-24 11:30:44 +0200

    jenkins: Attempt to fix FreeBSD distcheck
    Change-Id: I758a5590f3d964039363852f07f0967170fd9a28

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -16,4 +16,4 @@ cd ../../
 autoreconf --install --force
 PCAP_LIBS="-lpcap" PCAP_CFLAGS="" PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig ./configure --with-pcap-config=/bin/true
 PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig $MAKE $PARALLEL_MAKE
-PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig LD_LIBRARY_PATH=$PWD/deps/install/lib $MAKE distcheck
+DISTCHECK_CONFIGURE_FLAGS="--with-pcap-config=/bin/true" PCAP_LIBS="-lpcap" PCAP_CFLAGS="" PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig LD_LIBRARY_PATH=$PWD/deps/install/lib $MAKE distcheck
